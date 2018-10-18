46638919fa29577c2025675c64a5b8c5a41a09e7
darin-ellis7 <darin.ellis7@gmail.com>
2018-10-18 13:51:16 -0400

    Initial commit

---
+++ app/src/main/java/com/hfad/beeradvisor/FindBeerActivity.java
@@ -0,0 +1,36 @@
+package com.hfad.beeradvisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    //called when the button is clicked
+    public void onClickFindBeer(View view) {
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Display the selected item
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+
+}
